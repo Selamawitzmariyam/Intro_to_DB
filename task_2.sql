ad4dda067db2db2148345b193c2cc6115fc17b40
Selamawitzmariyam <selamawitgetu21@gmail.com>
2025-02-06 09:14:08 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,3 +1,12 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="Saintamanuel@28",
+    database="mydb"
+)
 CREATE DATABASE IF NOT EXISTS ALX_BOOK_STORE;
 USE ALX_BOOK_STORE;
 
